f3de8b22035134c44eb5aaa2de2628100b86e3c1
kisekitw <kisekitw@gmail.com>
2018-10-13 06:49:41 +0800

    Receive message from client and response it

--- NetMQDemo/Program.cs
+++ NetMQDemo/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetMQDemo
@@ -10,6 +13,23 @@ namespace NetMQDemo
     {
         static void Main(string[] args)
         {
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+
+                    Console.WriteLine("Received {0}", message);
+
+                    // processing the request
+                    Thread.Sleep(100);
+
+                    Console.WriteLine("Sending World");
+                    server.SendFrame("World");
+                }
+            }
         }
     }
 }
